cef7133328fab940b2d4ddbeaa55dff8b4d27f9d
Kory | Ryosuke Kondo <korygm33@gmail.com>
2023-11-30 21:12:42 +0900

    refactor: use `sendK` whenever possible

--- src/main/scala/io/github/kory33/experimental/recursiveeff/catseffect3/concurrent/ConcurrentEffectInterpretation.scala
+++ src/main/scala/io/github/kory33/experimental/recursiveeff/catseffect3/concurrent/ConcurrentEffectInterpretation.scala
@@ -6,21 +6,20 @@ import cats.~>
 import cats.effect.kernel.{Concurrent, Deferred => CEDeferred, Ref => CERef}
 import cats.arrow.FunctionK
 import cats.syntax.functor._
+import io.github.kory33.experimental.recursiveeff.util.EffUtils
 
-trait ConcurrentEffectInterpretation extends ConcurrentEffectCreation {
+trait ConcurrentEffectInterpretation extends ConcurrentEffectCreation with EffUtils {
   def delegateToConcurrent[F[_], E, R](
     using Concurrent[F],
     F |= R
-  ): ConcurrentEffect[R, _] ~> F =
-    val fToEffR: F ~> Eff[R, _] = FunctionK.lift([A] => (fa: F[A]) => send(fa))
-    FunctionK.lift([A] =>
-      (eff: ConcurrentEffect[R, A]) =>
-        (eff match
-          case ConcurrentEffect.Ref(a) =>
-            Concurrent[F].ref(a).map(_.mapK(fToEffR))
-          case d: ConcurrentEffect.Deferred[r, a] =>
-            Concurrent[F].deferred[a].map(_.mapK(fToEffR))
-        ): F[A])
+  ): ConcurrentEffect[R, _] ~> F = FunctionK.lift([A] =>
+    (eff: ConcurrentEffect[R, A]) =>
+      (eff match
+        case ConcurrentEffect.Ref(a) =>
+          Concurrent[F].ref(a).map(_.mapK(sendK[F, R]))
+        case d: ConcurrentEffect.Deferred[r, a] =>
+          Concurrent[F].deferred[a].map(_.mapK(sendK[F, R]))
+      ): F[A])
 
   def runConcurrentEffect[F[_], E, R, A, U](eff: Eff[R, A])(
     using Member.Aux[ConcurrentEffect[R, _], R, U],
--- src/main/scala/io/github/kory33/experimental/recursiveeff/catseffect3/spawn/SpawnEffectInterpretation.scala
+++ src/main/scala/io/github/kory33/experimental/recursiveeff/catseffect3/spawn/SpawnEffectInterpretation.scala
@@ -10,32 +10,32 @@ import cats.Functor
 import cats.syntax.all._
 import cats.effect.kernel.Outcome
 import io.github.kory33.experimental.recursiveeff.util.CEKernelExtensions
+import io.github.kory33.experimental.recursiveeff.util.EffUtils
 
 trait SpawnEffectInterpretation
     extends SpawnEffectCreation
-    with CEKernelExtensions {
+    with CEKernelExtensions
+    with EffUtils {
   def delegateToGenSpawn[F[_], E, R](runToF: Eff[R, _] ~> F)(
     using GenSpawn[F, E],
     F |= R
-  ): SpawnEffect[R, E, _] ~> F =
-    val fToEffR: F ~> Eff[R, _] = FunctionK.lift([A] => (fa: F[A]) => send(fa))
-    FunctionK.lift([A] =>
-      (eff: SpawnEffect[R, E, A]) =>
-        (eff match
-          case SpawnEffect.Start(eff) =>
-            GenSpawn[F, E].start(runToF(eff)).map(_.mapK(fToEffR))
-          case SpawnEffect.Never() =>
-            GenSpawn[F, E].never
-          case SpawnEffect.Cede() =>
-            GenSpawn[F, E].cede
-          case SpawnEffect.RacePair(left, right) =>
-            GenSpawn[F, E].racePair(runToF(left), runToF(right)).map(
-              _.bimap(
-                (outcomeLeft, fiberRight) =>
-                  (outcomeLeft.mapK(fToEffR), fiberRight.mapK(fToEffR)),
-                (fiberLeft, outcomeRight) =>
-                  (fiberLeft.mapK(fToEffR), outcomeRight.mapK(fToEffR))
-              )
+  ): SpawnEffect[R, E, _] ~> F = FunctionK.lift([A] =>
+    (eff: SpawnEffect[R, E, A]) =>
+      (eff match
+        case SpawnEffect.Start(eff) =>
+          GenSpawn[F, E].start(runToF(eff)).map(_.mapK(sendK[F, R]))
+        case SpawnEffect.Never() =>
+          GenSpawn[F, E].never
+        case SpawnEffect.Cede() =>
+          GenSpawn[F, E].cede
+        case SpawnEffect.RacePair(left, right) =>
+          GenSpawn[F, E].racePair(runToF(left), runToF(right)).map(
+            _.bimap(
+              (outcomeLeft, fiberRight) =>
+                (outcomeLeft.mapK(sendK[F, R]), fiberRight.mapK(sendK[F, R])),
+              (fiberLeft, outcomeRight) =>
+                (fiberLeft.mapK(sendK[F, R]), outcomeRight.mapK(sendK[F, R]))
             )
-        ): F[A])
+          )
+      ): F[A])
 }
